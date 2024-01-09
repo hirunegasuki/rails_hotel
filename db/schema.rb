52cdfa953827688d41fbbbce2742fbfa775cf3c3
hirunegasuki <147813709+hirunegasuki@users.noreply.github.com>
2024-01-09 23:38:52 +0900

    BDのカラム変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_12_12_125528) do
+ActiveRecord::Schema.define(version: 2024_01_07_084013) do
+
+  create_table "reservations", force: :cascade do |t|
+    t.string "name"
+    t.text "room_info"
+    t.integer "total_price"
+    t.integer "number"
+    t.date "check_in"
+    t.date "check_out"
+    t.string "image"
+    t.integer "user_id", null: false
+    t.integer "room_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["room_id"], name: "index_reservations_on_room_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.string "name"
+    t.text "room_info"
+    t.integer "price"
+    t.string "address"
+    t.string "image"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -29,8 +56,12 @@ ActiveRecord::Schema.define(version: 2023_12_12_125528) do
     t.integer "failed_attempts", default: 0, null: false
     t.string "unlock_token"
     t.datetime "locked_at"
+    t.text "profile"
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "reservations", "rooms"
+  add_foreign_key "reservations", "users"
 end
