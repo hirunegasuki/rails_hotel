060aab06a4f9dc680a77551914e8d0a198330cdb
hirunegasuki <147813709+hirunegasuki@users.noreply.github.com>
2024-01-09 23:30:50 +0900

    ルート変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,24 @@
 Rails.application.routes.draw do
-  devise_for :users
 
-  root to: 'hotels#index'
-  resources :hotels
-  resources :users
+  devise_for :users
+  get 'users/account'
+  get 'users/profile'
+  get 'users/profile_edit'
+  resources :users, only: [:edit, :update]
+
+  root to: 'rooms#index'
+
+  resources :rooms, only: [:index, :new, :show, :create] do
+    collection do
+      get 'search'
+      get 'own'
+    end
+  end
+
+  resources :reservations, only: [:index, :create] do
+    collection do
+      post :confirm
+    end
+  end
+
 end
