0df00a626871297e37c2dc458557b556855d0f98
hirunegasuki <147813709+hirunegasuki@users.noreply.github.com>
2024-01-09 23:28:32 +0900

    アカウント、プロフィールの編集機能作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,17 @@
 class ApplicationController < ActionController::Base
-    before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-    protected
-  
-    def configure_permitted_parameters
-      # devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-      added_attrs = [ :name, :email, :password, :password_confirmation ]
-      devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
-      devise_parameter_sanitizer.permit :account_update, keys: added_attrs
-    end
+  protected
+
+  # ログイン/ログアウト用
+  def configure_permitted_parameters
+    # devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    added_attrs = [ :name, :email, :password, :password_confirmation ]
+    #devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    #devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile, :avatar])
+  end
+
 end
